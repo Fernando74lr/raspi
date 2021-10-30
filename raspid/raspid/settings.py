3158aa9ffcf4209b73e8586463f43affdcc44191
AsielTrejo <AsielTrejo@Asiels-MacBook-Pro-2.local>
2021-10-30 18:49:09 -0500

    Users schema #1

--- raspid/raspid/settings.py
+++ raspid/raspid/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
     'users',
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'hackernews.schema.schema',
+}
---
+++ raspid/schema.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import get_user_model
+
+import graphene
+from graphene_django import DjangoObjectType
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        #components = graphene.String(required=True)
+        userType = graphene.String(required=True)
+
+    def mutate(self, info, username, password, userType):
+        user = get_user_model()(
+            username=username,
+            userType=userType,
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+class Query(graphene.ObjectType):
+    users = graphene.List(UserType)
+
+    def resolve_users(self, info):
+        return get_user_model().objects.all()

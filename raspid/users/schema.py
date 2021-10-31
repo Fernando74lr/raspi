42dd2e4f559b3b33bcc71d7b926311bd202a767e
Fernando74lr <flopezramirez@hotmail.com>
2021-10-30 20:13:51 -0500

    Update schema.py (email field)

--- raspid/users/schema.py
+++ raspid/users/schema.py
@@ -15,12 +15,12 @@ class CreateUser(graphene.Mutation):
     class Arguments:
         username = graphene.String(required=True)
         password = graphene.String(required=True)
-        userPrivileges = graphene.String(required=True)
+        email = graphene.String(required=True)
 
-    def mutate(self, info, username, password, userPrivileges):
+    def mutate(self, info, username, password, email):
         user = get_user_model()(
             username=username,
-            userPrivileges=userPrivileges,
+            email=email,
         )
         user.set_password(password)
         user.save()

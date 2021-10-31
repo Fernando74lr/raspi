2c85f8e175a798fd5c2b3a8c107f68a08afce208
TheIszer <theiszer@gmail.com>
2021-10-30 21:01:52 -0500

    DB created #9

--- raspid/components/models.py
+++ raspid/components/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Component(models.Model):
+    name = models.TextField(blank=True)
+    cType = models.TextField(blank=True)
+    value = models.TextField(blank=True)
+    unit = models.TextField(blank=True)
+    #token = models.TextField(blank=True)
+    #id_user = models.TextField(blank=True)

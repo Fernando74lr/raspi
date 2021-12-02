92198cda6af2cfb8f493c34c265139791d079e53
Fernando López Ramírez <flopezramirez@hotmail.com>
2021-12-01 19:27:25 -0600

    Added logs schema

--- raspid/logs/models.py
+++ raspid/logs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Log(models.Model):
+    value = models.TextField(blank=True)
+    component = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
---
+++ raspid/logs/schema.py
@@ -0,0 +1,41 @@
+import graphene
+from graphene_django import DjangoObjectType
+from components.schema import ComponentType
+from .models import Log
+
+class LogType(DjangoObjectType):
+    class Meta:
+        model = Log
+
+class Query(graphene.ObjectType):
+    logs = graphene.List(LogType)
+
+    def resolve_logs(self, info, **kwargs):
+        return Log.objects.all()
+
+class CreateLog(graphene.Mutation):
+    id = graphene.Int()
+    value = graphene.String()
+    component = graphene.Field(ComponentType)
+    
+    class Arguments:
+        value = graphene.String()
+
+    def mutate(self, info, value):
+        component = info.context.component or None
+
+        log = Log(
+            value = value,
+            component=component,
+        )
+        log.save()
+
+        return CreateLog( 
+            id=log.id,
+            value=log.value,
+            component=log.component,
+        )
+
+class Mutation(graphene.ObjectType):
+    create_log = CreateLog.Field()
+
